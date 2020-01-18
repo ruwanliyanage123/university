e7e8c11dca1c320efc4f8e9fc928290e739d8e31
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2020-01-19 03:07:20 +0530

    Add method to get all lectures in service

--- backend/src/main/java/com/example/backend/admin/services/LecturerService.java
+++ backend/src/main/java/com/example/backend/admin/services/LecturerService.java
@@ -2,9 +2,11 @@ package com.example.backend.admin.services;
 
 import com.example.backend.admin.Repositories.LecturerRepository;
 import com.example.backend.admin.models.Lecturer;
+import com.example.backend.admin.models.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 /**
  * service class for lecturer
@@ -21,5 +23,13 @@ import org.springframework.stereotype.Service;
          */
         public Lecturer addLecturer(Lecturer lecturer){
             return lecturerRepository.save(lecturer);
+        }
+
+        /**
+         * to invoke find all method in jpa
+         * @return list of lecturer
+         */
+        public List<Lecturer> getAllLecturer(){
+            return lecturerRepository.findAll();
         }
     }
