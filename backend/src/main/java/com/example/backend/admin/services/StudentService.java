dde64955e3f3cc831e061aa1f2d0d953946d79a0
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2020-01-18 15:33:10 +0530

    Add student service

---
+++ backend/src/main/java/com/example/backend/admin/services/StudentService.java
@@ -0,0 +1,14 @@
+package com.example.backend.admin.services;
+
+import com.example.backend.admin.Repositories.StudentReposiroty;
+import com.example.backend.admin.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StudentService {
+    @Autowired
+    StudentReposiroty studentReposiroty;
+
+    public Student addStudent(Student student){
+        return studentReposiroty.save(student);
+    }
+}
