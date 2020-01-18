374743f2ba647e0d64c18a1ef45857fddc8d35b0
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2020-01-18 20:30:18 +0530

    Add subject model

---
+++ backend/src/main/java/com/example/backend/admin/models/Subject.java
@@ -0,0 +1,43 @@
+package com.example.backend.admin.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "subject")
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int subjectId;
+
+    private String subjectName;
+
+    private int numberOfCredits;
+
+    public int getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+}
