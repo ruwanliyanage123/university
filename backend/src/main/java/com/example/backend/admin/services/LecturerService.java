f3960c1c3e48a4b6528b2c0d354d6832ef792003
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2020-01-19 00:53:04 +0530

    Add method to add lecturer in lecturer service

---
+++ backend/src/main/java/com/example/backend/admin/services/LecturerService.java
@@ -0,0 +1,26 @@
+package com.example.backend.admin.services;
+
+import com.example.backend.admin.Repositories.LecturerRepository;
+import com.example.backend.admin.models.Lecturer;
+import com.example.backend.admin.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * service class for lecturer
+ */
+@Service
+public class LecturerService {
+    @Autowired
+    LecturerRepository lecturerRepository;
+
+    /**
+     * to invoke save method in jpa
+     * @param lecturer new lecturer
+     * @return inserted lecturer
+     */
+    public Lecturer addLecturer(Lecturer lecturer){
+        return lecturerRepository.save(lecturer);
+    }
+}
