db64f2195d5161171872a88100a1b293b9c5d5f5
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2020-01-18 20:55:19 +0530

    Add method to get all subjects in subject service

--- backend/src/main/java/com/example/backend/admin/services/SubjectService.java
+++ backend/src/main/java/com/example/backend/admin/services/SubjectService.java
@@ -6,6 +6,8 @@ import com.example.backend.admin.models.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SubjectService {
     @Autowired
@@ -14,10 +16,17 @@ public class SubjectService {
     /**
      * to invoke save method in jpa
      * @param subject
-     * @return inserted student
+     * @return inserted subject
      */
     public Subject addStudent(Subject subject){
         return subjectRepository.save(subject);
     }
 
+    /**
+     * to invoke find all method in jpa
+     * @return list of subject
+     */
+    public List<Subject> getAllStudent(){
+        return subjectRepository.findAll();
+    }
 }
