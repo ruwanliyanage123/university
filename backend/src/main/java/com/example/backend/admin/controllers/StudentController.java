a116deea66c1df3776d225e713ebf66aa4a38fb4
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2020-01-18 16:35:11 +0530

    Resolve error in slf4g loggers

--- backend/src/main/java/com/example/backend/admin/controllers/StudentController.java
+++ backend/src/main/java/com/example/backend/admin/controllers/StudentController.java
@@ -4,17 +4,19 @@ import com.example.backend.admin.models.Student;
 import com.example.backend.admin.services.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController @RequestMapping("/students") public class StudentController {
-    private static Logger logger = LoggerFactory.getLogger(StudentService.class);
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private static Logger logger = LoggerFactory.getLogger(StudentController.class);
     StudentService studentService;
 
-    @PostMapping("/add") public Student addStudent(@RequestBody Student student) {
+    @PostMapping("/add")
+    public Student addStudent(@RequestBody Student student) {
         Student student1 = null;
         try {
             student1 = studentService.addStudent(student);
