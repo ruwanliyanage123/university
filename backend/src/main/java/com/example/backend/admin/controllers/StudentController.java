5bd41ccccd2e7b61b544c36e30b0a3772d9aecb0
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2020-01-18 17:51:19 +0530

    Create service object by factory method

--- backend/src/main/java/com/example/backend/admin/controllers/StudentController.java
+++ backend/src/main/java/com/example/backend/admin/controllers/StudentController.java
@@ -4,17 +4,27 @@ import com.example.backend.admin.models.Student;
 import com.example.backend.admin.services.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * controller class for the student
+ */
 @RestController
 @RequestMapping("/students")
 public class StudentController {
     private static Logger logger = LoggerFactory.getLogger(StudentController.class);
+    @Autowired
     StudentService studentService;
 
+    /**
+     * to insert a new student
+     * @param student new student
+     * @return insert student
+     */
     @PostMapping("/add")
     public Student addStudent(@RequestBody Student student) {
         Student student1 = null;
--- backend/src/main/java/com/example/backend/admin/services/StudentService.java
+++ backend/src/main/java/com/example/backend/admin/services/StudentService.java
@@ -3,12 +3,21 @@ package com.example.backend.admin.services;
 import com.example.backend.admin.Repositories.StudentReposiroty;
 import com.example.backend.admin.models.Student;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+/**
+ * service class for student
+ */
+@Service
 public class StudentService {
-
     @Autowired
     StudentReposiroty studentReposiroty;
 
+    /**
+     * to invoke save method in jpa
+     * @param student new student
+     * @return inserted student
+     */
     public Student addStudent(Student student){
         return studentReposiroty.save(student);
     }
