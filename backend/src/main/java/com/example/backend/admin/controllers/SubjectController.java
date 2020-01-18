b9eb1c1108efcfcd0082ecfd88ff40109c315421
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2020-01-18 22:53:35 +0530

    Add method to update  subject in subject controll

--- backend/src/main/java/com/example/backend/admin/controllers/SubjectController.java
+++ backend/src/main/java/com/example/backend/admin/controllers/SubjectController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -82,5 +83,25 @@ public class SubjectController {
             logger.error("Student not in database. re check the student id number", e);
         }
         return subjectService.deleteSubject(subject);
+    }
+
+    /**
+     * to update subject
+     * @param subjectId subject id
+     * @param subject subject
+     * @return updated subject
+     */
+    @PutMapping("/update/{subjectId}")
+    public Subject updateSubject(@PathVariable int subjectId, @RequestBody Subject subject){
+        Subject subject1 = null;
+        try {
+            subject1 = subjectService.getOneSubject(subjectId);
+        } catch (SubjectNotFoundException e) {
+            logger.error("subject not in database. re check the subject id number", e);
+        }
+        assert subject1 != null;
+        subject1.setSubjectName(subject.getSubjectName());
+        subject1.setNumberOfCredits(subject.getNumberOfCredits());
+        return subjectService.updateSubject(subject1);
     }
 }
