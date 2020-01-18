e24ff548895b70b965eefae66c1e0f21505727ca
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2020-01-19 00:38:12 +0530

    Add staff abstract class

---
+++ backend/src/main/java/com/example/backend/admin/models/Staff.java
@@ -0,0 +1,84 @@
+package com.example.backend.admin.models;
+
+public abstract class Staff {
+
+    private int staffID;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String mobile;
+
+    private String email;
+
+    private double salary;
+
+    private String city;
+
+    private String street;
+
+    public int getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(int staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+}
