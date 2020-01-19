8759211b4cc648265f7b440febf708ec434d33b5
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2020-01-19 10:18:50 +0530

    Add method get one instructor in service

--- backend/src/main/java/com/example/backend/admin/services/InstructorService.java
+++ backend/src/main/java/com/example/backend/admin/services/InstructorService.java
@@ -1,6 +1,8 @@
 package com.example.backend.admin.services;
 
 import com.example.backend.admin.Repositories.InstructorRepository;
+import com.example.backend.admin.exceptions.InstructorNotFoundException;
+import com.example.backend.admin.exceptions.LecturerNotFoundException;
 import com.example.backend.admin.models.Instructor;
 import com.example.backend.admin.models.Lecturer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,5 +33,16 @@ public class InstructorService {
      */
     public List<Instructor> getAllInstructor(){
         return instructorRepository.findAll();
+    }
+
+    /**
+     * to invoke find single method in jpa
+     * @param instructorId instructor id
+     * @return selected instructor
+     * @throws InstructorNotFoundException when instructor not in database
+     */
+    public Instructor getOneInstructor(int instructorId) throws InstructorNotFoundException {
+        return instructorRepository.findById(instructorId)
+                .orElseThrow(()->new InstructorNotFoundException());
     }
 }
